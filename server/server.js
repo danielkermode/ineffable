0c8f1aa104eba7b30ad5dfd64694a9658dfbf6ec
danielkermode <danielkermode@hotmail.co.nz>
2016-07-15 11:12:09 +1200

    all basic routes

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/users', users);
-// app.use('/api/profiles', profiles);
-// app.use('/api/statuses', statuses);
+app.use('/api/profiles', profiles);
+app.use('/api/statuses', statuses);
 
 const server = app.listen(process.env.PORT || 3000, () =>  {
   console.log(`Server ready on port ${server.address().port}`);
